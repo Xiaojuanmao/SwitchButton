9c1ed5ef850c9ee486564fee800e1d118d877996
Xiaojuanmao <daque@hustunique.com>
2016-09-05 00:01:32 +0800

    init repository

---
+++ switchbutton/src/main/java/feel/life/wonderful/library_switchbutton/SwitchButton.java
@@ -0,0 +1,32 @@
+package feel.life.wonderful.library_switchbutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author : xiaoxiaoda
+ *         date: 16-9-4
+ *         email: daque@hustunique.com
+ */
+
+public class SwitchButton extends View {
+
+  public SwitchButton(Context context) {
+    this(context, null);
+  }
+
+  public SwitchButton(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public SwitchButton(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+
+  }
+
+}
