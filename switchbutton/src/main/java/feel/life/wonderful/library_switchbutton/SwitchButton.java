018f63c70daf6f88d1f376243c6b0c7293de6de0
queda <queda@xiaomi.com>
2016-09-07 00:08:40 +0800

    commit

--- switchbutton/src/main/java/feel/life/wonderful/library_switchbutton/SwitchButton.java
+++ switchbutton/src/main/java/feel/life/wonderful/library_switchbutton/SwitchButton.java
@@ -3,6 +3,9 @@ package feel.life.wonderful.library_switchbutton;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,6 +23,13 @@ public class SwitchButton extends View {
   private float mCircleShadowRadius;
   private float mBgWidth;
   private float mBgHeight;
+
+  private Paint mBgPaint;
+  private Paint mCirclePaint;
+  private Paint mShadowPaint;
+
+  private PointF mLeftCirclePoint;
+  private PointF mRightCirclePoint;
 
   private static final int DEFAULT_BG_COLOR = 0xf85b50;
   private static final int DEFAULT_CIRCLE_COLOR = 0x82aeff;
@@ -44,12 +54,29 @@ public class SwitchButton extends View {
   private void init(Context context, AttributeSet attrs) {
     TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SwitchButton);
     mBgColor = typedArray.getColor(R.styleable.SwitchButton_switchButtonBgColor, DEFAULT_BG_COLOR);
-    mCircleColor = typedArray.getColor(R.styleable.SwitchButton_switchButtonBgColor, DEFAULT_CIRCLE_COLOR);
+    mCircleColor = typedArray.getColor(R.styleable.SwitchButton_switchButtonCircleColor, DEFAULT_CIRCLE_COLOR);
     mCircleRadius = typedArray.getDimension(R.styleable.SwitchButton_switchButtonCircleRadius, DEFAULT_CIRCLE_RADIUS);
     mCircleShadowRadius = typedArray.getDimension(R.styleable.SwitchButton_switchButtonCircleShadowRadius, DEFAULT_CIRCLE_SHADOW_RADIUS);
     mBgWidth = typedArray.getDimension(R.styleable.SwitchButton_switchButtonBgWidth, DEFAULT_BG_WIDTH);
     mBgHeight = typedArray.getDimension(R.styleable.SwitchButton_switchButtonBgHeight, DEFAULT_BG_HEIGHT);
     typedArray.recycle();
+
+    initPaints();
+  }
+
+  private void initPaints() {
+    mBgPaint = new Paint();
+    mBgPaint.setAntiAlias(true);
+    mBgPaint.setColor(mBgColor);
+
+    mCirclePaint = new Paint();
+    mCirclePaint.setAntiAlias(true);
+    mCirclePaint.setColor(mCircleColor);
+
+    mShadowPaint = new Paint();
+
+    mLeftCirclePoint = new PointF();
+    mRightCirclePoint = new PointF();
   }
 
   @Override
@@ -61,19 +88,44 @@ public class SwitchButton extends View {
     int widthSize = MeasureSpec.getSize(widthMeasureSpec);
 
     if (widthMode == MeasureSpec.EXACTLY) {
+      // 处理match_parent和dimen
       width = widthSize;
     } else {
+      // 处理wrap_content
 
+      width = widthSize;
     }
 
     int heightMode = MeasureSpec.getMode(heightMeasureSpec);
     int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
+    if (heightMode == MeasureSpec.EXACTLY) {
+      height = heightSize;
+    } else {
+
+      height = heightSize;
+    }
+
+    mLeftCirclePoint.set(mBgHeight / 2, mBgHeight / 2);
+    mRightCirclePoint.set(mBgWidth - mBgHeight / 2, mBgHeight / 2);
+
+    setMeasuredDimension(width, height);
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
+    drawBackground(canvas);
+    drawCircle(canvas);
+  }
+
+  private void drawBackground(Canvas canvas) {
+    canvas.drawCircle(mLeftCirclePoint.x, mLeftCirclePoint.y, mBgHeight / 2, mBgPaint);
+    canvas.drawRect(mBgHeight / 2, 0, mBgWidth - mBgHeight / 2, mBgHeight, mBgPaint);
+    canvas.drawCircle(mRightCirclePoint.x, mRightCirclePoint.y, mBgHeight / 2, mBgPaint);
+  }
+
+  private void drawCircle(Canvas canvas) {
+    canvas.drawCircle(mLeftCirclePoint.x, mLeftCirclePoint.y, mCircleRadius, mCirclePaint);
   }
 
 
